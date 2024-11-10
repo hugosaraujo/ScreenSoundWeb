ccc5b710bd37e1828081f0305180d0e4512a2bac
hugo de araujo <hugo.saraujo@tutanota.com>
2024-11-09 21:35:18 -0300

    Adição de migraiton e fazendo a inserção de dados nas tabelas do projeto.

---
+++ ScreenSound/Migrations/20241109232218_PopularTabela.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Artistas", new string[] {"Nome", "Bio", "FotoPerfil" }, new object[] { "Nile Rodgers & CHIC", "Nile Rodgers & Chic: a combinação perfeita entre funk, R&B e disco, criando uma trilha sonora inesquecível para diversas gerações.", "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" });
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "Charli XCX", "A rainha do hyperpop: Charli XCX reinventa a música pop com beats acelerados, vocais distorcidos e performances eletrizantes.", "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "Jessie J", "Com uma voz poderosa e única, Jessie J é uma das maiores cantoras da atualidade, capaz de emocionar em cada nota.", "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "David Bowie", "O camaleão do rock: David Bowie, ícone da música, reinventou-se constantemente, explorando diversos estilos e desafiando as convenções.", "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "Nadson, O Ferinha", "O fenômeno do arrocha: Nadson, O Ferinha, com suas músicas apaixonantes e ritmo contagiante, é a trilha sonora perfeita para os apaixonados.", "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "Pablo", "O rei do arrocha: Pablo conquista o público com seu timbre marcante e músicas que embalam as noites de romance.", "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "Taylor Swift", "A loirinha que transformou a indústria musical, passando do country para o pop e quebrando recordes a cada álbum.", "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "Amy Winehouse", "A artista que mesclou elementos de jazz, R&B e soul, criando um som sofisticado e atemporal.", "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Artistas");
+        }
+    }
+}
---
+++ ScreenSound/Migrations/20241109235845_PopulandoTabelaMusicas.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulandoTabelaMusicas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento"}, new object[] { "Good Times", 2013 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "1999", 2014 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Price Tag", 2011 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Heroes", 1977 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Aquarela", 2004 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Se eu não te amasse tanto assim", 2003});
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Shake it Off ", 2014 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Back to Black ", 2006});
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Flashlight", 2015});
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Video Games", 2011});
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
